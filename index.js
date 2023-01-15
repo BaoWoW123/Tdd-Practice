78b7af9b30e5a6013d2558531d7cd8b2610cd7d1
BaoWoW <baonguyen9392@gmail.com>
2023-01-14 18:55:18 -0800

    Create All Tests & Functions
    Created several functions: capitalize, reverseString, calculator, caesarCipher, analyzeArray.
    Each function has test cases to check viability and potential bugs with various inputs.

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
-function sum(a, b) {
-  return a + b;
+function capitalize(str) {
+  if (!isNaN(str[0])) return str;
+  return str[0].toUpperCase() + str.slice(1);
 }
 
-module.exports = sum;
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+const calculator = {
+  add(a, b) {
+    if (isNaN(a) || isNaN(b)) return NaN;
+    return a + b;
+  },
+  subtract(a, b) {
+    if (isNaN(a) || isNaN(b)) return NaN;
+    return a - b;
+  },
+  divide(a, b) {
+    if (isNaN(a) || isNaN(b)) return NaN;
+    else if (b == 0) return NaN;
+    return a / b;
+  },
+  multiply(a, b) {
+    if (isNaN(a) || isNaN(b)) return NaN;
+    return a * b;
+  },
+};
+
+function caesarCipher(str) {
+  let arr = [];
+  for (let i = 0; i < str.length; i++) {
+    //checks if character is a letter
+    if (str[i].toUpperCase() !== str[i].toLowerCase()) {
+      let current = str.charCodeAt(i) + 1;
+      if (current === 97);
+      arr.push(String.fromCharCode(current));
+    } else arr.push(str[i]);
+  }
+  return arr.join("");
+}
+
+function analyzeArray(array) {
+  if (!Array.isArray(array)) return "Not an array";
+  let object = {};
+  object.average = array.reduce((a, b) => a + +b, 0) / array.length;
+  object.length = array.length;
+  object.max = Math.max(...array);
+  object.min = Math.min(...array);
+  return object;
+}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray };
--- index.test.js
+++ index.test.js
@@ -1,5 +1,104 @@
-const sum = require('./index');
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+  analyzeArray,
+} from "./index";
 
-test('add 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+//Capitalize function
+test("capitalize first character of string all lower case", () => {
+  expect(capitalize("hello, world.")).toBe("Hello, world.");
+});
+
+test("capitalize first character of string all upper case", () => {
+  expect(capitalize("hELLO, WORLD.")).toBe("HELLO, WORLD.");
+});
+
+test("don't capitalize with first character beig a number ", () => {
+  expect(capitalize("1hello, world.")).toBe("1hello, world.");
+});
+
+//Reverse string function
+
+test("reverse string", () => {
+  expect(reverseString("hello world")).toBe("dlrow olleh");
+});
+
+test("reverse string of numbers", () => {
+  expect(reverseString("12345")).toBe("54321");
+});
+
+//Calculator function
+
+test("add posite numbers", () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+test("add negative numbers", () => {
+  expect(calculator.add(-1, -2)).toBe(-3);
+});
+
+test("add non-integer with number to equal NaN", () => {
+  expect(calculator.add(-1, "t")).toBe(NaN);
+});
+
+test("divide 1 / 2 to equal .5", () => {
+  expect(calculator.divide(1, 2)).toBe(0.5);
+});
+
+test("divide negative numbers ", () => {
+  expect(calculator.divide(1, -2)).toBe(-0.5);
+});
+
+test("divide from 0 to equal 0", () => {
+  expect(calculator.divide(0, 2)).toBe(0);
+});
+
+test("divide by 0 to equal NaN instead of Infinity", () => {
+  expect(calculator.divide(2, 0)).toBe(NaN);
+});
+
+test("multiply 2 * 2 to equal 4", () => {
+  expect(calculator.multiply(2, 2)).toBe(4);
+});
+
+//Caesar cipher function
+test("cipher string shifted by 1 letter", () => {
+  expect(caesarCipher("thisismypassword")).toBe("uijtjtnzqbttxpse");
+});
+
+test("cipher string with punctuation", () => {
+  expect(caesarCipher("this, is, my password.")).toBe("uijt, jt, nz qbttxpse.");
+});
+
+test("cipher string with non-letter characters", () => {
+  expect(caesarCipher("th1$, 1s, my p@55w0rd!")).toBe("ui1$, 1t, nz q@55x0se!");
+});
+
+//Analyze array function
+test("analyze array and return object containing average, min, max, & length", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
+
+test("analyze array with some integers as strings", () => {
+  expect(analyzeArray([1, "8", 3, "4", 2, "6"])).toStrictEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
+
+test("analyze array that is a string", () => {
+  expect(analyzeArray("notAnArray")).toStrictEqual("Not an array");
+});
+
+test("analyze object that is not an array", () => {
+  expect(analyzeArray({ a: 1, b: 2 })).toStrictEqual("Not an array");
 });
